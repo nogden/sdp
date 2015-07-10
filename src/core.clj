25e727304a47638d7f7428bbe4f8b43b53b14a35
Nick Ogden <nick@nickogden.org>
2015-07-10 11:55:24 +0100

    Fix parsing of simple fields

--- src/core.clj
+++ src/core.clj
@@ -41,14 +41,14 @@
 ;; media-level value.
 
 (def test-sdp-string
-  "v=0
+  "v=n
    o=jdoe 2890844526 2890842807 IN IP4 10.47.16.5
    s=SDP Seminar
    i=A Seminar on the session description protocol
    u=http://www.example.com/seminars/sdp.pdf
    e=j.doe@example.com (Jane Doe)
    c=IN IP4 224.2.17.12/127
-   t=2873397496 2873404696
+   t=2873397496 2873404696value
    r=7d 1h 0 25h
    a=recvonly
    m=audio 49170 RTP/AVP 0
@@ -66,7 +66,7 @@
   {:string identity
    :integer #(Integer/parseInt %)
    :numeric-string (throw-if-nil "String '%v' is not alphanumeric"
-                    #(re-matches #"[0-9|A-Z|a-z]*" %))
+                                 #(re-matches #"[0-9|A-Z|a-z]*" %))
    :instant #(Integer/parseInt %)
    :duration identity
    :connection-address identity})
@@ -74,7 +74,7 @@
 (def error-fns
   "The set of error handlers."
   {:default-to (fn [sdp line-name value arg]
-                 (assoc sdp line-name value))
+                 (assoc sdp line-name arg))
    :debug (fn [sdp line-name value arg]
             (log/debug (string/replace arg "%v" value)) sdp)
    :warn (fn [sdp line-name value arg]
@@ -89,9 +89,9 @@
   "The rules used to parse SDP fields."
   {"v" [:version
         :integer
-        [:on-fail [:default-to 0
-                   :warn "Invalid version '%v', assuming default of '0'."]]
-        :required]
+        {:on-fail [[:default-to 0]
+                   [:warn "Invalid version '%v', assuming default of '0'."]]
+         :options #{:required}}]
    "o" [:origin
         {:separator #"\s+"
          :fields [:username        [:string #{}]
@@ -103,17 +103,17 @@
                   :address-type    [:string #{"IP4" "IP6"}
                                     :debug "Non-standard address-type '%v'"]
                   :unicast-address [:string]]}
-        [:on-fail [:error "Invalid origin line (o=)."]]
-        :required]
+        {:on-fail [[:error "Invalid origin line (o=)."]]
+         :options #{:required}}]
    "s" [:name
         :string
-        [:on-fail [:default-to " "
-                   :warn "Invalid empty session name, assuming default of '%d'."]]
-        :required]
+        {:on-fail [[:default-to " "]
+                   [:warn "Invalid empty session name, assuming default of '%d'."]]
+         :options #{:required}}]
    "i" [:information :string]
    "u" [:url :string]
-   "e" [:email :string :collate]
-   "p" [:phone :string :collate]
+   "e" [:email :string {:options #{:collate}}]
+   "p" [:phone :string {:options #{:collate}}]
    "c" [:connection
         {:separator #"\s+"
          :fields [:network-type       [:string #{"IN"}
@@ -122,22 +122,22 @@
                                        :debug "Non-standard address-type '%v'"]
                   :connection-address [:connection-address  ;; Must pass full field.
                                        :error "Invalid connection-address '%v'"]]}
-        [:on-fail [:error "Invalid connection line (c=)."]]]
+        {:on-fail [[:error "Invalid connection line (c=)."]]}]
    "b" [:bandwidth
         {:separator #":"
          :fields [:bandwidth-type [:string #{"CT" "AS"}
                                    :debug "Non-standard bandwidth-type '%v'"]
                   :bandwidth [:integer
                               :error "Bandwidth value '%v' is not an integer"]]}
-        [:on-fail [:warn "Invalid bandwidth line (b=)."]]]
+        {:on-fail [[:warn "Invalid bandwidth line (b=)."]]}]
    "t" [:timing
         {:separator #"\s+"
          :fields [:start-time [:instant
                                :error "Start time '%v' is not a time"]
                   :end-time   [:instant
                                :error "End time '%v' is not a time"]]}
-        [:on-fail [:warn "Invalid timing line (t=)."]]
-        :collate]
+        {:on-fail [[:warn "Invalid timing line (t=)."]]
+         :options #{:collate}}]
    "r" [:repeat
         {:separator #"\s+"
          :fields [:repeat-interval [:duration
@@ -147,34 +147,37 @@
                   :offsets-from-start [:duration
                                        :error "Invalid offsets list '%v'"
                                        :collate]]}
-        [:on-fail [:warn "Invalid repeat line (r=)."]]
-        :collate]})
+        {:on-fail [[:warn "Invalid repeat line (r=)."]]
+         :options #{:collate}}]})
 
 (defn add-error [e & [sdp line]]
   (update-in sdp [:errors] (fnil conj []) {:message (.getMessage e)
                                            :line line
                                            :data (ex-data e)}))
 
-(defn parse-fields [] nil)
+(defn parse-fields
+  "Extract fields from line according to rule."
+  [line [_ line-type & options]]
+  :field-parsing-not-implemented)
 
 (defn parse-line
   "Parse line accroding to the rule."
-  [sdp line [line-name line-type & options]]
+  [sdp line [line-name line-type {options :options} :as rule]]
   (let [result (cond
-                (map? line-type) (parse-fields line line-type)
+                (map? line-type) (parse-fields line rule)
                 :else ((line-type parse-fns) line))]
     (assoc sdp line-name result)))
 
 (with-handler! #'parse-line
   "Handle parsing errors according to the error rules."
   Exception
-  (fn [e [sdp line [line-name _ [_ _ error-spec]]]]
+  (fn [e & [sdp line [line-name _ {error-spec :on-fail}]]]
     (letfn [(handle-errors [sdp [op param]]
-             ((op error-fns) sdp line param))]
+             ((op error-fns) sdp line-name line param))]
       (reduce handle-errors sdp error-spec))))
 
 (defn parse-sdp [sdp line]
-   (let [[line-type value] (string/split line #"=" 2)
+  (let [[line-type value] (map string/trim (string/split line #"=" 2))
          rule (parsing-rules line-type)]
      (if (some? rule)
        (parse-line sdp value rule)
@@ -189,7 +192,7 @@
   [sdp-string]
   (reduce parse-sdp {} (string/split-lines sdp-string)))
 
-;; (parse test-sdp-string)
+(parse test-sdp-string)
 
 (defn emit
   "Emits the SDP description string for the provided media session
